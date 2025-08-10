a520f56dad957e461a6d533293bc39c289a240b3
ArtZul <a.zulpikarov@hezzl.com>
2025-08-10 18:02:16 +0300

    fix

--- room-editor.lua
+++ room-editor.lua
@@ -2,12 +2,12 @@ local dia = Dialog("Room editor")
 local image = app.image
 local ww = image.width
 local hh = image.height
-local data = {data = {}}
+local room = {data = {}}
 for it in image:pixels() do
     local pixelValue = it()
     local indexX = it.x + 1
     local indexY = it.y + 1
-    local d = data.data
+    local d = room.data
     if d[indexY] == nil then
         d[indexY] = {}
     end
@@ -15,11 +15,11 @@ for it in image:pixels() do
 end
 
 local function copy()
-    app.clipboard.text = json.encode(data)
+    app.clipboard.text = json.encode(room)
     dia:close()
 end
 
-dia:entry{id="area", text=json.encode(data), focus=true}
+dia:entry{id="area", text=json.encode(room), focus=true}
 dia:separator()
 dia:button{id="copy", text="Copy", onclick=copy, selected=true, focus=true}
 dia.bounds = Rectangle(app.window.width * 0.5 - 64, app.window.height * 0.5 - 32, 128, 64)
