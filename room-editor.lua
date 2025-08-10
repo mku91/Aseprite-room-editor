abcd46be0bb7080a965905b3b78f7fa5507dbb2f
ArtZul <a.zulpikarov@hezzl.com>
2025-08-10 17:50:22 +0300

    init-commit

---
+++ room-editor.lua
@@ -0,0 +1,27 @@
+local dia = Dialog("Room editor")
+local image = app.image
+local ww = image.width
+local hh = image.height
+local data = {data = {}}
+for it in image:pixels() do
+    local pixelValue = it()
+    local indexX = it.x + 1
+    local indexY = it.y + 1
+    local d = data.data
+    if d[indexY] == nil then
+        d[indexY] = {}
+    end
+    d[indexY][indexX] = pixelValue
+end
+
+local function copy()
+    app.clipboard.text = json.encode(data)
+    dia:close()
+end
+
+dia:entry{id="area", text=json.encode(data), focus=true}
+dia:separator()
+dia:button{id="copy", text="Copy", onclick=copy, selected=true, focus=true}
+dia.bounds = Rectangle(app.window.width * 0.5 - 64, app.window.height * 0.5 - 32, 128, 64)
+
+dia:show()
